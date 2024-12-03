a3af8a5ed8307ed95c46f98e47bc169018ba08db
eltonticonsulte <elton@ticonsulte.com.br>
2024-12-03 13:18:09 -0300

    fix: devops

---
+++ devops/config_local.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Use /tmp directory for sessions which should be writable
+SESSION_DB_PATH = "/tmp/sessions"
+STORAGE_DIR = "/tmp/storage"
+AZURE_CREDENTIAL_CACHE_DIR = "/tmp/azurecredentialcache"
+
+# Email configuration
+CHECK_EMAIL_DELIVERABILITY = False
+ALLOW_SPECIAL_EMAIL_DOMAINS = []
