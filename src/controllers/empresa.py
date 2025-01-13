7b641c36950cde17d22fe0221a5c6bab79342a56
eltonticonsulte <elton@ticonsulte.com.br>
2025-01-13 13:26:08 -0300

    fix(empresa): add rule

--- src/controllers/empresa.py
+++ src/controllers/empresa.py
@@ -3,11 +3,13 @@ from typing import List
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.responses import JSONResponse
 from src.interfaces import InterfaceEmpresaService, InterfaceUserService
-from .core import auth2_admin, get_service_user, get_service_empresa
+from src.enums import UserRole
+from .core import auth2_admin, get_service_user, get_service_empresa, rule_require
 from ..dto import (
     CreateEmpresaRequest,
     GetEmpresaResponse,
     UpdateEmpresaRequest,
+    AuthUserResponse,
 )
 
 router = APIRouter()
@@ -16,13 +18,9 @@ router = APIRouter()
 @router.post("/create", status_code=201, response_model=GetEmpresaResponse)
 async def create(
     request: CreateEmpresaRequest,
-    token: str = Depends(auth2_admin),
     service: InterfaceEmpresaService = Depends(get_service_empresa),
-    auth: InterfaceUserService = Depends(get_service_user),
+    user: AuthUserResponse = Depends(rule_require(UserRole.ADMIN)),
 ):
-    user = auth.current_user(token)
-    if not user:
-        raise HTTPException(401, detail="Unauthorized")
     try:
         result: GetEmpresaResponse = service.create(request)
         return result
@@ -32,16 +30,10 @@ async def create(
 
 @router.get("/all", status_code=200, response_model=List[GetEmpresaResponse])
 async def get_all(
-    token: str = Depends(auth2_admin),
-    auth: InterfaceUserService = Depends(get_service_user),
+    user: AuthUserResponse = Depends(rule_require(UserRole.ADMIN)),
     service: InterfaceEmpresaService = Depends(get_service_empresa),
 ):
-    try:
-        auth.current_user(token)
-    except Exception as error:
-        raise HTTPException(
-            401, detail=f"Unauthorized: {error}", headers={"WWW-Authenticate": "Bearer"}
-        )
+
     try:
         result: List[GetEmpresaResponse] = service.get_all()
         return result
@@ -52,14 +44,9 @@ async def get_all(
 @router.get("/get/{empresa_id}", status_code=200, response_model=GetEmpresaResponse)
 async def get_by_id(
     empresa_id: int,
-    token: str = Depends(auth2_admin),
-    auth: InterfaceUserService = Depends(get_service_user),
+    user: AuthUserResponse = Depends(rule_require(UserRole.ADMIN)),
     service: InterfaceEmpresaService = Depends(get_service_empresa),
 ):
-    try:
-        auth.current_user(token)
-    except Exception:
-        raise HTTPException(401, detail="Unauthorized")
     try:
         result: GetEmpresaResponse = service.get_by_id(empresa_id)
         return result
@@ -71,16 +58,10 @@ async def get_by_id(
 async def update(
     empresa_id: int,
     request: UpdateEmpresaRequest,
-    token: str = Depends(auth2_admin),
-    auth: InterfaceUserService = Depends(get_service_user),
+    user: AuthUserResponse = Depends(rule_require(UserRole.ADMIN)),
     service: InterfaceEmpresaService = Depends(get_service_empresa),
 ):
-    try:
-        auth.current_user(token)
-    except Exception as error:
-        raise HTTPException(
-            401, detail=f"Unauthorized: {error}", headers={"WWW-Authenticate": "Bearer"}
-        )
+
     try:
         result: GetEmpresaResponse = service.update(empresa_id, request)
         return result
@@ -91,16 +72,9 @@ async def update(
 @router.delete("/delete/{empresa_id}", status_code=200)
 async def delete(
     empresa_id: int,
-    token: str = Depends(auth2_admin),
-    auth: InterfaceUserService = Depends(get_service_user),
+    user: AuthUserResponse = Depends(rule_require(UserRole.ADMIN)),
     service: InterfaceEmpresaService = Depends(get_service_empresa),
 ):
-    try:
-        auth.current_user(token)
-    except Exception as error:
-        raise HTTPException(
-            401, detail=f"Unauthorized: {error}", headers={"WWW-Authenticate": "Bearer"}
-        )
     try:
         service.delete(empresa_id)
         return JSONResponse(status_code=200, content={"status": "ok"})
