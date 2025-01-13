670d35b662073fa21f398753a14a51ca750975dc
eltonticonsulte <elton@ticonsulte.com.br>
2025-01-13 13:30:14 -0300

    fix(cameras): add rule acess

--- src/controllers/camera.py
+++ src/controllers/camera.py
@@ -8,8 +8,9 @@ from src.dto import (
     CreateCameraRequest,
     GetCameraResponse,
     UpdateCameraRequest,
+    AuthUserResponse,
 )
-from .core import auth2_admin, get_service_camera, get_service_user
+from .core import auth2_admin, get_service_camera, get_service_user, rule_require
 from ..enums import UserRole
 
 router = APIRouter()
@@ -18,15 +19,10 @@ router = APIRouter()
 @router.post("/create", status_code=201, response_model=GetCameraResponse)
 async def create(
     request: CreateCameraRequest,
-    token: uuid.UUID = Depends(auth2_admin),
-    auth: InterfaceUserService = Depends(get_service_user),
+    user: AuthUserResponse = Depends(rule_require(UserRole.ADMIN)),
     service: InterfaceCameraService = Depends(get_service_camera),
 ):
-    try:
-        auth.current_user(token)
-        auth.premissao(UserRole.ADMIN)
-    except Exception as error:
-        raise HTTPException(401, detail=str(error))
+
     try:
         result: GetCameraResponse = service.create(request)
         return result
@@ -34,29 +30,12 @@ async def create(
         raise HTTPException(500, detail=str(error))
 
 
-@router.get("/status")
-async def get_status(
-    token: str = Depends(auth2_admin),
-    auth: InterfaceUserService = Depends(get_service_user),
-    service: InterfaceCameraService = Depends(get_service_camera),
-):
-    try:
-        service.current_user(get_service_user)
-    except Exception as error:
-        raise HTTPException(401, detail=str(error))
-    return JSONResponse(status_code=200, content={"status": "ok", "name": "nameuser"})
-
-
 @router.get("/all", status_code=200, response_model=List[GetCameraResponse])
 async def get_all(
-    token: str = Depends(auth2_admin),
-    auth: InterfaceUserService = Depends(get_service_user),
+    user: AuthUserResponse = Depends(rule_require(UserRole.ADMIN)),
     service: InterfaceCameraService = Depends(get_service_camera),
 ):
-    try:
-        auth.current_user(token)
-    except Exception as error:
-        raise HTTPException(401, detail=str(error))
+
     try:
         result: List[GetCameraResponse] = service.get_all()
         return result
@@ -68,14 +47,10 @@ async def get_all(
 async def update(
     channel_id: uuid.UUID,
     request: UpdateCameraRequest,
-    token: str = Depends(auth2_admin),
-    auth: InterfaceUserService = Depends(get_service_user),
+    user: AuthUserResponse = Depends(rule_require(UserRole.ADMIN)),
     service: InterfaceCameraService = Depends(get_service_camera),
 ):
-    try:
-        auth.current_user(token)
-    except Exception as error:
-        raise HTTPException(401, detail=str(error))
+
     try:
         result: GetCameraResponse = service.update(channel_id, request)
         return result
@@ -86,14 +61,9 @@ async def update(
 @router.delete("/delete/{channel_id}", status_code=200)
 async def delete(
     channel_id: uuid.UUID,
-    token: str = Depends(auth2_admin),
-    auth: InterfaceUserService = Depends(get_service_user),
+    user: AuthUserResponse = Depends(rule_require(UserRole.ADMIN)),
     service: InterfaceCameraService = Depends(get_service_camera),
 ):
-    try:
-        auth.current_user(token)
-    except Exception as error:
-        raise HTTPException(401, detail=str(error))
 
     try:
         service.delete(channel_id)
