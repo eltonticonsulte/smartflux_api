7394e547aacd56b5daaa4f4ea0c486c01c6f920a
eltonticonsulte <elton@ticonsulte.com.br>
2025-01-23 14:20:42 -0300

    fix: update

--- src/external/aws_websocket.py
+++ src/external/aws_websocket.py
@@ -2,6 +2,7 @@
 from logging import getLogger
 import json
 import boto3
+from botocore.config import Config
 from core import get_settings
 from src.interfaces import InterfaceObserver
 from src.dto import EventCountSendWebsocket
@@ -12,11 +13,17 @@ class DataEventWebSocketNotifier(InterfaceObserver):
         self.log = getLogger("DataEventWebSocketNotifier")
         self.log.info(" start get_stactic_count_event_websocket")
         self.endpoint = get_settings().WEBSOCKET_ENDPOINT
+        config = Config(
+            retries={"max_attempts": 10, "mode": "standard"},
+            connect_timeout=5,
+            read_timeout=5,
+        )
         # session = boto3.Session(profile_name="smartflux")
         self.client = boto3.client(
             "apigatewaymanagementapi",
             endpoint_url=self.endpoint,
             region_name=get_settings().AWS_REGION,
+            config=config,
         )
 
     async def update(self, data: EventCountSendWebsocket, connection_id: str) -> None:
