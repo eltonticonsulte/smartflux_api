b22bde216e5286bb8251f272a0dff42cd5f02200
eltonticonsulte <elton@ticonsulte.com.br>
2025-01-30 09:02:07 -0300

    bump: version 0.0.8 → 0.0.9

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from core import get_settings
 from src.database import DBConnectionHandler
 from src.compose import create_app
 
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 os.environ["__VERSION__"] = __version__
 
 LoggerConfig()
