2dc117d1d06236ef86413f3a0567fc2b748a179f
eltonticonsulte <elton@ticonsulte.com.br>
2025-02-25 19:40:39 -0300

    fix: remove label

--- src/controllers/visitor.py
+++ src/controllers/visitor.py
@@ -46,32 +46,3 @@ async def get_visitor(
     except Exception as error:
         log.error("error", exc_info=error)
         raise HTTPException(500, detail=str(error))
-
-
-@router.get("/label", status_code=200, response_model=ResponseGrupDataCode)
-async def get_visitor(
-    start_date: datetime.date = Query(...),
-    end_date: Optional[datetime.date] = None,
-    grup: Optional[DataFilterTimer] = Query(DataFilterTimer.UNDEFINED),
-    zone: Optional[str] = Query(None),
-    device: Optional[str] = Query(None),
-    user: UserPermissionAccessDTO = Depends(rule_require(UserRule.FILIAL)),
-    storage: InterfaceStorageService = Depends(get_service_storage),
-):
-    """
-    Busca data dado um periodo de uma filial
-    start_day: 2025-01-01
-    end_day: 2025-01-31
-    """
-    try:
-        data = RequestVisitor(
-            start_data=start_date,
-            end_data=end_date,
-            grup=grup,
-            zone=zone,
-            device=device,
-        )
-        return storage.get_count_visitor(user.filial_id, data)
-    except Exception as error:
-        log.error("error", exc_info=error)
-        raise HTTPException(500, detail=str(error))
