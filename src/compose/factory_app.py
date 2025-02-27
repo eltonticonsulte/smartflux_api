3a73b88aefacb285d23fc8d5a2d12161605fcf56
eltonticonsulte <elton@ticonsulte.com.br>
2025-02-27 15:44:46 -0300

    fix: remove scheduler

--- src/compose/factory_app.py
+++ src/compose/factory_app.py
@@ -2,18 +2,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.executors.pool import ThreadPoolExecutor
-from apscheduler.triggers.cron import CronTrigger
+
+# from apscheduler.schedulers.background import BackgroundScheduler
+# from apscheduler.executors.pool import ThreadPoolExecutor
+# from apscheduler.triggers.cron import CronTrigger
 from src.database import DBConnectionHandler
 from src.api import base_ruter
 from core import get_settings
 from .factory_service import FactoryService
 
-executors = {
-    "default": ThreadPoolExecutor(5),  # Até 5 tarefas em paralelo
-}
-scheduler = BackgroundScheduler(executors=executors)
+# executors = {
+#    "default": ThreadPoolExecutor(5),  # Até 5 tarefas em paralelo
+# }
+# scheduler = BackgroundScheduler(executors=executors)
 
 
 @asynccontextmanager
@@ -52,10 +53,10 @@ def create_app(log, version):
     app = FastAPI(title=get_settings().PROJECT_NAME, version=version, lifespan=lifespan)
     app.include_router(base_ruter, prefix=get_settings().API_V1_STR)
     permsion(app)
-    trigger = CronTrigger(
-        hour=0,
-        minute=0,
-    )
-    scheduler.add_job(event, trigger, max_instances=1)
+    # trigger = CronTrigger(
+    #    hour=0,
+    #    minute=0,
+    # )
+    # scheduler.add_job(event, trigger, max_instances=1)
 
     return app
